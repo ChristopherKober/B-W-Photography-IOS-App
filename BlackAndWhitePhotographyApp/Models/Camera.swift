bc69cbd1e9eeabc9a50fa94833ad4b9676636b86
ChristopherKober <christopherkober2018@u.northwestern.edu>
2017-03-26 16:54:01 -0500

    Added MVC shell to project

---
+++ BlackAndWhitePhotographyApp/Models/Camera.swift
@@ -0,0 +1,13 @@
+//
+//  Camera.swift
+//  BlackAndWhitePhotographyApp
+//
+//  Created by Kent on 3/26/17.
+//  Copyright © 2017 Kent Kober. All rights reserved.
+//
+
+import Foundation
+
+class Camera {
+
+}
